0117a5c51724bfec1a1f75984e93096e9884f075
Yousef-Al-Zeer <iyousefzeer@gmail.com>
2024-08-06 20:55:29 +0300

    create custom text field

---
+++ lib/views/widgets/custom_text_field.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/constants.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+        hintText: 'Tilte',
+        border: boardBuilder(),
+        enabledBorder: boardBuilder(),
+        focusedBorder: boardBuilder(kPrimaryColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder boardBuilder([color]) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(
+        color: color ?? Colors.white,
+      ),
+    );
+  }
+}
