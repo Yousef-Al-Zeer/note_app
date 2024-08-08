5d83a05d51658004a782e2dc9e8482f4b87cf208
Yousef-Al-Zeer <iyousefzeer@gmail.com>
2024-08-08 20:11:51 +0300

    create add note cubit

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -10,7 +10,7 @@ class NotesViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+      padding: EdgeInsets.symmetric(horizontal: 24),
       child: const Column(
         children: [
           SizedBox(
